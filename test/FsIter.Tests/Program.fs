5c080e035d3ab079a1d5c0b27d74d005b151addf
David N <davidcalieln@gmail.com>
2022-03-28 14:02:13 -0700

    Add test to check length after applying filter

--- test/FsIter.Tests/Program.fs
+++ test/FsIter.Tests/Program.fs
@@ -2,22 +2,17 @@
 
 open System.Collections.Immutable
 open Expecto
-open Expecto.ExpectoFsCheck
 open FsIter
 
 [<EntryPoint>]
 let main argv =
     testList "all" [
         testList "map" [
-            testProperty "same length" <| fun (elements: int[]) (mapping: int -> int) ->
-                let actual =
-                    Iter.from elements
-                    |> Iter.map mapping
-                    |> Iter.length
+            testProperty "has same length" <| fun (elements: int[]) (mapping: _ -> int) ->
+                let actual = Iter.map mapping (Iter.from elements)
+                elements.Length = Iter.length actual
 
-                elements.Length = actual
-
-            testProperty "mapping with id returns equivalent sequence" <| fun (elements: int[]) ->
+            testProperty "equivalent when identity is used" <| fun (elements: int[]) ->
                 let expected = ImmutableArray.Create(items = elements)
                 let mutable actual = ImmutableArray.CreateBuilder(elements.Length)
 
@@ -26,6 +21,12 @@ let main argv =
                 |> Iter.appendToCollection actual
 
                 expected = actual.MoveToImmutable()
+        ]
+
+        testList "filter" [
+            testProperty "length less than or equal to original" <| fun (elements: int[]) (filter: _ -> bool) ->
+                let actual = Iter.filter filter (Iter.from elements)
+                Iter.length actual <= elements.Length
         ]
     ]
     |> runTestsWithCLIArgs [] argv
