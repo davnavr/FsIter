1c276d7f22969caf694bb7820b2540d536b773d5
David N <davidcalieln@gmail.com>
2022-03-28 16:34:51 -0700

    Add benchmark project

---
+++ test/FsIter.Benchmarks/Map.fs
@@ -0,0 +1,8 @@
+﻿namespace FsIter.Benchmarks
+
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+
+[<MemoryDiagnoser>]
+type Map =
+    let items = [|  |]
---
+++ test/FsIter.Benchmarks/Program.fs
@@ -0,0 +1,8 @@
+﻿module FsIter.Benchmarks.Program
+
+type Marker = struct end
+
+[<EntryPoint>]
+let main argv =
+    BenchmarkDotNet.Running.BenchmarkSwitcher.FromAssembly(typeof<Marker>.Assembly).Run(argv) |> ignore
+    0
