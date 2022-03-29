403a639ae7563b4a91e417195b3fb64f1eaea649
David N <davidcalieln@gmail.com>
2022-03-28 22:29:40 -0700

    Add benchmark for conversion to immutable array

---
+++ test/FsIter.Benchmarks/ToImmutableArray.fs
@@ -0,0 +1,23 @@
+﻿namespace FsIter.Benchmarks
+
+open System.Collections.Immutable
+open BenchmarkDotNet.Attributes
+open FsIter
+
+[<MemoryDiagnoser>]
+type ToImmutableArray () =
+    [<Benchmark(Baseline = true)>]
+    [<ArgumentsSource("Elements")>]
+    member _.Create(elements: int[]) = ImmutableArray.Create(items = elements)
+
+    [<Benchmark>]
+    [<ArgumentsSource("Elements")>]
+    member _.Iter(elements: int[]) = Iter.toImmutableArray (Iter.fromArray elements)
+
+    member _.Elements: int[][] =
+        [|
+            [||]
+            [| 1 |]
+            [| 1..100 |]
+            [| 1..1000 |]
+        |]
