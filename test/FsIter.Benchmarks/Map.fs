b2cc1ff0ffb125fbdab7db032a4b83e3bd42cc16
David N <davidcalieln@gmail.com>
2022-03-28 20:07:36 -0700

    Add benchmark for Map

--- test/FsIter.Benchmarks/Map.fs
+++ test/FsIter.Benchmarks/Map.fs
@@ -1,8 +1,30 @@
 ﻿namespace FsIter.Benchmarks
 
 open BenchmarkDotNet.Attributes
-open BenchmarkDotNet.Running
+open FsIter
 
 [<MemoryDiagnoser>]
-type Map =
-    let items = [|  |]
+type Map () =
+    let mapping n = n + 42
+    let nothing _ = ()
+
+    [<Benchmark(Baseline = true)>]
+    [<ArgumentsSource("Elements")>]
+    member _.SeqMap(elements: int[]) =
+        Seq.map mapping elements |> Seq.iter nothing
+
+    [<Benchmark>]
+    [<ArgumentsSource("Elements")>]
+    member _.IterMap(elements: int[]) =
+        Iter.from elements
+        |> Iter.map mapping
+        |> Iter.iter nothing
+
+    member _.Elements: int[][] =
+        [|
+            [||]
+            [| 1 |]
+            [| 1..100 |]
+            [| 1..1000 |]
+            [| 1..10000 |]
+        |]
